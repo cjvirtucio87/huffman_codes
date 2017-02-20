bc04310f69cb3a9eceed151821bfb136f2704de1
cjvirtucio87 <cjv287@gmail.com>
2017-02-20 16:39:01 -0500

    test(huffman): even length
    - tested for even length datasets

--- huffman_test.go
+++ huffman_test.go
@@ -1,6 +1,7 @@
 package huffman
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -11,6 +12,8 @@ var data = []datum{
 	{key: rune('d'), val: 5},
 	{key: rune('e'), val: 2},
 }
+
+var evenData = append(data, datum{key: rune('f'), val: 4})
 
 func TestRemoveFirstTwo(t *testing.T) {
 	var dataset []datum
@@ -27,6 +30,39 @@ func TestRemoveFirstTwo(t *testing.T) {
 		// must not mutate array pointed to by data
 		t.Error("Expected ", 1, ", got ", len(dataset))
 	}
+}
+
+// helper for TestGetMid
+func testMids(dataset []datum, t *testing.T) {
+	// with original dataset
+	fullMid := getMid(0, len(dataset))
+	leftMid := getMid(0, getMid(0, len(dataset)))
+	rightMid := getMid(getMid(0, len(dataset)), len(dataset))
+
+	if len(dataset)%2 != 0 {
+		if fullMid != 3 {
+			t.Error("Expected ", 3, ", got ", fullMid)
+		} else if leftMid != 2 {
+			t.Error("Expected ", 2, ", got ", leftMid)
+		} else if rightMid != 5 {
+			t.Error("Expected ", 5, ", got ", rightMid)
+		}
+	} else {
+		if fullMid != 4 {
+			t.Error("Expected ", 4, ", got ", fullMid)
+		} else if leftMid != 3 {
+			t.Error("Expected ", 3, ", got ", leftMid)
+		} else if rightMid != 6 {
+			t.Error("Expected ", 6, ", got ", rightMid)
+		}
+	}
+}
+
+func TestGetMid(t *testing.T) {
+	fmt.Println("huffman.getMid should calculate the mid index in the range of data.")
+
+	testMids(data, t)
+	testMids(evenData, t)
 }
 
 // func TestFindNode(t *testing.T) {
