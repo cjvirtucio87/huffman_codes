87f25a78ae8827d518a5c33df2f1745aa505cb4e
cjvirtucio87 <cjv287@gmail.com>
2017-02-20 09:42:53 -0500

    test(huffman): tweaked test
    - tweaked test to accommodate bst helper

--- huffman_test.go
+++ huffman_test.go
@@ -1,58 +1,34 @@
 package huffman
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/cjvirtucio87/bst"
+)
+
+var data = []bst.Datum{
+	{Key: rune('a'), Val: 3},
+	{Key: rune('b'), Val: 5},
+	{Key: rune('c'), Val: 7},
+	{Key: rune('d'), Val: 5},
+	{Key: rune('e'), Val: 2},
+}
 
 func TestRemoveFirstTwo(t *testing.T) {
-	data := []datum{
-		{key: rune('a'), val: 3},
-		{key: rune('b'), val: 5},
-		{key: rune('c'), val: 7},
-	}
-
 	sliced, orig := removeFirstTwo(data)
 
-	if sliced[0].key != rune('a') {
-		t.Error("Expected ", rune('a'), ", got ", sliced[0].key)
-	} else if sliced[1].key != rune('b') {
-		t.Error("Expected ", rune('b'), ", got ", sliced[1].key)
-	} else if len(orig) != 3 {
+	if sliced[0].Key != rune('a') {
+		t.Error("Expected ", rune('a'), ", got ", sliced[0].Key)
+	} else if sliced[1].Key != rune('b') {
+		t.Error("Expected ", rune('b'), ", got ", sliced[1].Key)
+	} else if len(orig) != 5 {
 		// must not mutate array pointed to by data
-		t.Error("Expected ", 3, ", got ", len(orig))
+		t.Error("Expected ", 5, ", got ", len(orig))
 	}
 }
 
 func TestSearch(t *testing.T) {
-	root := node{
-		key:    rune('a'),
-		val:    1,
-		parent: nil,
-		left:   nil,
-		right:  nil,
-	}
-
-	data := []datum{
-		{key: rune('b'), val: 3},
-		{key: rune('c'), val: 5},
-		{key: rune('d'), val: 7},
-	}
-
-	for _, d := range data {
-		newNode := node{
-			key:    d.key,
-			val:    d.val,
-			parent: &root,
-			left:   nil,
-			right:  nil,
-		}
-
-		if d.val < root.val {
-			root.left = &newNode
-		} else if d.val > root.val {
-			root.right = &newNode
-		}
-
-		root = newNode
-	}
+	root := bst.CreateTree(data)
 
 	result := search(rune('b'), &root)
 
